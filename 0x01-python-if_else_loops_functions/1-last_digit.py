56e52823b5da906d8f5d6a27599dc730b657d970
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-12-21 22:12:55 -0500

    number 1 again

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,12 +7,13 @@ if number > 0 and (number % 10) < 6:
 elif (number % 10) > 6:
     str1 = 'and is greater than 5'
     str1a = 'Last digit of'
-    print(("{} {} is {} {}".format(str1a, number, number % 10, str1)))
+    print(("{:s} {:d} is {:d} {:s}".format(str1a, number, number % 10, str1)))
 elif number % 10 == 0:
     str2 = 'and is  0'
     str1a = 'Last digit of'
-    print(("{} {} is {} {}".format(str1a, number, number % 10, str2)))
+    print(("{:s} {:d} is {:d} {:s}".format(str1a, number, number % 10, str2)))
 else:
     str3 = 'and is less than 6 and not 0'
     str1b = 'Last digit of'
-    print(("{} {} is {} {}".format(str1b, number, number % -10, str3)))
+    mol = -number
+    print(("{:s} {:d} is {:d} {:s}".format(str1b, number, -(mol % 10), str3)))
