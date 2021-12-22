e18ef5147a2a7311c3889398267ed46983c6418d
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-12-21 22:51:50 -0500

    number 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-     modul = number % 10 if number > 0 else  number *-1 % 10
-     print('{:d}'.format(modul), end="")
-     return(modul)
+    if number > 0:
+     modul = number % 10  
+    else:
+     modul = number *-1 % 10
+    print('{:d}'.format(modul), end="")
+    return(modul)
