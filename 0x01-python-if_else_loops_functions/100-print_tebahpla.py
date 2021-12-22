42b8411af31927d7c73c707992476d1ab7579a1c
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-12-21 23:31:12 -0500

    number 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,4 @@ for i in range(122, 96, -1):
     if i % 2 == 0:
         print("{}".format(chr(i)), end="")
     else:
-        i = i - 32
-        print("{}".format(chr(i)), end="")
+        print("{}".format(chr(i - 32)), end="")
