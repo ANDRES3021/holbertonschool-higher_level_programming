c543d340d936db1c0b15268a6fd09bdc2286f891
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-12-21 22:29:47 -0500

    number 1 again

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0 and (number % 10) < 6:
-    str = 'and is less than 6 and not 0'
-    print("Last digit of {:d} is {:d} {:s}".format(number, number % 10, str))
-elif (number % 10) > 6:
-    str1 = 'and is greater than 5'
-    str1a = 'Last digit of'
-    print(("{:s} {:d} is {:d} {:s}".format(str1a, number, number % 10, str1)))
-elif number % 10 == 0:
-    str2 = 'and is  0'
-    str1a = 'Last digit of'
-    print(("{:s} {:d} is {:d} {:s}".format(str1a, number, number % 10, str2)))
+if number > 0:
+    modul = number % 10
+    number2 = number
 else:
-    str3 = 'and is less than 6 and not 0'
-    str1b = 'Last digit of'
-    mol = -number
-    print(("{:s} {:d} is {:d} {:s}".format(str1b, number, -(mol % 10), str3)))
+    modul = (((number * (- 1)) % 10) * -1)
+    number2 = number
+if modul == 0:
+    print("Last digit of {} is {} and is 0".format(number, modul))
+elif modul > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, modul))
+else:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number2, modul))
