53d74a168822f2c6ee734e11acdbc275313b3bf0
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-03 22:50:16 -0500

    number 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    if my_string is None:
-        return
     str_new = ""
     for i in my_string:
         if i != 'c' and i != 'C':
