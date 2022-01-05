b0314ea1767e0e5123c98236f6337d21f46f1834
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-04 21:49:41 -0500

    number 0 and README

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix_tmp = []
+    for x in matrix:
+        matrix_tmp.append(list( map(lambda x: x*2, x) ))
+    return(matrix_tmp)
+
