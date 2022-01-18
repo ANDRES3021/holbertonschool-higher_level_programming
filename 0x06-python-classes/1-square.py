ac849a1e169c644ce9038ec932c8457a86697d6c
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-18 16:49:31 -0500

    number1 fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,4 +6,4 @@ class Square():
     """init method that receives the initialization parameters"""
     def __init__(self, size):
         """Expression to initialize our class"""
-        self__size = size
+        self.__size = size
