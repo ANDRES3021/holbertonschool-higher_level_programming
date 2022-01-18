6d860aaf880c97e25235fa1a93ab3a0b3639e68e
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-18 16:46:30 -0500

    number1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class keyword to define the class"""
+
+
+class Square:
+    """init method that receives the initialization parameters"""
+    def __init__(self, size):
+        """Expression to initialize our class"""
+        self__size = size
