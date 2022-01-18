b22ff3182eff86a6fc557b3c8bb5c12a792862e2
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-18 17:33:19 -0500

    number2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,10 +6,10 @@ class Square():
     """init method that receives the initialization parameters"""
     def __init__(self, size=0):
         """"I check ValueError and TypeError exceptions"""
-        if  isinstance(size, int) == False:
-            raise TypeError('size must be an integer')
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         else:
             """Expression to initialize our instance variable"""
             self.__size = size
