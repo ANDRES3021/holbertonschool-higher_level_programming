e10f69ce58b70bc6b0818d77b4e6735e44623563
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-17 19:46:41 -0500

    number3

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from itertools import count
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except(TypeError, ValueError):
+            i += 1
+            continue
+    print('')
+    return (count)
