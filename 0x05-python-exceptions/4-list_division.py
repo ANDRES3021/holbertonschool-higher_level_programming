bb1aa4e3e4fd9f6497b2aeb8739e11c7a172f667
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-17 20:49:40 -0500

    number4 fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,10 +7,13 @@ def list_division(my_list_1, my_list_2, list_length):
             continue
         except(ZeroDivisionError):
             print('division by 0')
+            result.append(0)
         except(IndexError):
             print('out of range')
+            result.append(0)
         except(TypeError):
             print('wrong type')
+            result.append(0)
         finally:
             pass
-    return result
+    return(result)
