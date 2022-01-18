dfe83e51621e0250061d10bb3e46007457066e9f
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-18 17:46:59 -0500

    number3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class keyword to define the class"""
+
+
+class Square():
+    """init method that receives the initialization parameters"""
+    def __init__(self, size=0):
+        """"I check ValueError and TypeError exceptions"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            """Expression to initialize our instance variable"""
+            self.__size = size
+
+    def area(self):
+        return(self.__size**2)
