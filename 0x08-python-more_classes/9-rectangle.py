0c626e7dc47a1df12ee598801cf1a1907ab8c00c
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-24 22:05:53 -0500

    number 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -79,3 +79,8 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         return(rect_1 if rect_1.area() >= rect_2.area() else rect_2)
+
+    @classmethod
+    def square(cls, size=0):
+        """initializes a new square instance"""
+        return cls(size, size)
