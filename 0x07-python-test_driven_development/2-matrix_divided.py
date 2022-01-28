afc07cdd9532dae48e308af5d05a7565bd97b8d7
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-01-27 23:01:17 -0500

    fixed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
+"""Module to divide a matrix
+Divide each element of a matrix
+of numbers by a number
+"""
+
+
+from operator import length_hint
+
+
 def matrix_divided(matrix, div):
-    error_message = 'matrix must be a matrix (list of lists) of integers/floats'
+    """
+    Function that divides all elements of an array
+    """
+    error_message = "matrix must be a matrix(list of lists) of integers/floats"
     if not matrix:
         raise TypeError(error_message)
     if not isinstance(matrix, list):
-        raise TypeError(error_message) 
+        raise TypeError(error_message)
     for lists_iterator in matrix:
-        if not isinstance (lists_iterator, list):
+        length_lists = len(lists_iterator)
+        if not isinstance(lists_iterator, list):
             raise TypeError(error_message)
         for items_lists in lists_iterator:
-            if not isinstance(items_lists, int) and not isinstance(items_lists, float):
+            if not isinstance(items_lists, (int, float)):
                 raise TypeError(error_message)
     for lists_iterator in matrix:
         if len(lists_iterator) == 0:
             raise TypeError(error_message)
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError('div must be a number')
-    if not all(len(lists_iterator) == len(matrix[0]) for lists_iterator in matrix):
+    if not all((length_lists) == len(matrix[0])for length_lists in matrix):
         raise TypeError('Each row of the matrix must have the same size')
     if div == 0:
         raise ZeroDivisionError('division by zero')
-    return [[round(items_lists / div, 2) for items_lists in lists_iterator] for lists_iterator in matrix ]
+    return [[round(items_lists/div, 2) for items_lists in lists_iterator] for lists_iterator in matrix]
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+Module say_my_name 
+first_name : First Name
+last_name : Middle Name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Prints a string with the two input parameters.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
