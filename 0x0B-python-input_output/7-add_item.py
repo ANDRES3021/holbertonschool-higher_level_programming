33a0481bc31772f92c384090ed136457ef0f7c16
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-02-01 23:54:12 -0500

    number7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""import module"""
+import json
+import sys
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+read_file = __import__("0-read_file").read_file
+
+filename = "add_item.json"
+li = []
+
+try:
+    li = load_from_json_file(filename)
+except Exception:
+    li = []
+for i in sys.argv[1:]:
+    li.append(i)
+save_to_json_file(li, filename)
