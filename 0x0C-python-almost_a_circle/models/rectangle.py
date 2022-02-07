4f098befef287cb2fd91d550dc5bd336da7b20f8
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-02-07 15:32:18 -0500

    update again :(

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,13 +120,13 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 if key == "width":
-                    self.__width = value
+                    self.width = value
                 if key == "height":
-                    self.__height = value
+                    self.height = value
                 if key == "x":
-                    self.__x = value
+                    self.x = value
                 if key == "y":
-                    self.__y = value
+                    self.y = value
                 if key == "id":
                     self.id = value
 
