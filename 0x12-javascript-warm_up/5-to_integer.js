380fe199185e941f120fe0e869137db02e40a79c
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-04-04 18:52:19 -0500

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-if (!parseInt(process.argv[2]))
-{
+if (!parseInt(process.argv[2])) {
     console.log('Not a number');
-}
-else
-{
+} else {
     console.log('My number: ' + parseInt(process.argv[2]));
 }
