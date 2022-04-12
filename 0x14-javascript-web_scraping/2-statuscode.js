db7c8092af444966745d07dbb9cb628f37d8373e
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-04-12 18:06:12 -0500

    status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,7 +4,7 @@ const url = process.argv[2];
 request(url, (err, response) => {
   if (err) {
     console.log(err);
-  } else  {
+  } else {
     console.log('code: ' + response.statusCode);
   }
 });
