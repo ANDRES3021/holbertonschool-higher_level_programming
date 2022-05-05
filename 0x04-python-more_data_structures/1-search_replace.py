c449c83d758124f9403bc8438a757a9e6a78f3ea
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-05-04 19:50:32 -0500

    README fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return(list(map(lambda x: replace if x is search else x, my_list)))
+    return(x is list(map(lambda x: replace if search else x, my_list)))
