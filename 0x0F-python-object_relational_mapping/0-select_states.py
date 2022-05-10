eeb8fee98ff7f5abc719536b281dabe0dde808ec
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-05-10 14:59:40 -0500

    lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
+        )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for states in cur.fetchall():
+        print(states)
+    cur.close()
+    db.close()
