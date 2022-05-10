687c9f58ac5726b5d3ee3e0ad67461958039f610
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-05-10 16:58:15 -0500

    script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in arguments and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument."""
+"""script that lists all cities from the database hbtn_0e_4_usa"""
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
@@ -9,11 +8,12 @@ if __name__ == "__main__":
         )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (argv[4],)
+        "SELECT cities.id, cities.name, states.name\
+            FROM cities INNER JOIN states ON cities.state_id=states.id\
+                ORDER BY cities.state_id ASC"
     )
 
     for states in cur.fetchall():
-        if states[1] == argv[4]:
-            print(states)
+        print(states)
     cur.close()
     db.close()
