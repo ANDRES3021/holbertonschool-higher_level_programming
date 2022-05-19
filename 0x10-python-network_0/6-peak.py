c28e964517a9e03f494f08a722eb7d6b1cb2142d
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-05-18 21:58:40 -0500

    number 6 fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,5 @@ def find_peak(list_of_integers):
     """ Returns a peak number from a unsorted list"""
     if len(list_of_integers) < 1:
         return None
-
     list_of_integers.sort()
     return list_of_integers[-1]
