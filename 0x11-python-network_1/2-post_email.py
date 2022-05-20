133fae7433cb3509226b35b0a2738c6d9bc93f90
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-05-20 17:50:17 -0500

    Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    value = {'email': email}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('utf-8')
+    with urllib.request.urlopen(url, data) as response:
+        answer = response.read().decode('utf-8')
+    print(answer)
