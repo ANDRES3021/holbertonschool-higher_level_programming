bb67a0480b8fd74eae20279892866e62edf5a23c
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-05-20 17:00:45 -0500

    number 0 fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,14 +3,10 @@
 
 if __name__ == "__main__":
     import urllib.request
-
-    try:
-        url = "https://intranet.hbtn.io/status"
-        url_intranet = urllib.request.urlopen(url)
-        html = url_intranet.read()
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode("utf-8", "replace")))
-    except Exception as err:
-        print("Error: ", err)
