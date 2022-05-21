aa5fcd187261cef5f5ad8ddc97ad46bfe9d06fdb
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-05-20 21:31:04 -0500

    all

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    Displays the body of the response
+    """
+    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(r.json().get("id"))
